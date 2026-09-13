32b53d40968fb8386591ae2f2402bde8ffe31ae8
varonvgc <varonvgc@gmail.com>
2026-09-13 12:45:10 +0900

    fix(camera): OBSのバグを誘発するframeRate制約を削除し、pamo3と完全に同じ制約に変更

--- scripts/auto_mode_controller.js
+++ scripts/auto_mode_controller.js
@@ -351,13 +351,10 @@
         video: this.currentVideoDeviceId ? {
           deviceId: { exact: this.currentVideoDeviceId },
           width: { ideal: 1920, min: 1280 },
-          height: { ideal: 1080, min: 720 },
-          frameRate: { ideal: 60, min: 30 },
-          colorSpace: 'srgb'
+          height: { ideal: 1080, min: 720 }
         } : {
           width: { ideal: 1920, min: 1280 },
-          height: { ideal: 1080, min: 720 },
-          colorSpace: 'srgb'
+          height: { ideal: 1080, min: 720 }
         }
       };
 
